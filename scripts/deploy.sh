897a42a1484efbcaf402b2f5ccbf4fdda2358ef0
majorro <andrabaratrum007@gmail.com>
2023-12-05 23:41:27 +0300

    chore: deploy script

---
+++ scripts/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+git fetch
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+    git pull
+    docker compose up -d --no-deps --build pibot
+else
+    echo "???"
+fi
